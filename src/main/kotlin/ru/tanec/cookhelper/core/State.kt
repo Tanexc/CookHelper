1c97586e0a472347dfdfb94fa192b5a0632e0175
Arthur231456 <artur.231456@gmail.com>
2022-10-02 23:26:13 +0300

    Status added. State changes

--- src/main/kotlin/ru/tanec/cookhelper/core/State.kt
+++ src/main/kotlin/ru/tanec/cookhelper/core/State.kt
@@ -2,18 +2,25 @@ package ru.tanec.cookhelper.core
 
 
 sealed class State<T>(
-    val data: T? = null
+    val data: T? = null,
+    val message: String,
+    val status: Int
 ) {
 
-    class Success<T>(data: T? = null):  State<T>(data = data)
+    class Success<T>(data: T? = null, message: String = "success", status: Int) :
+        State<T>(data = data, message = message, status = status)
 
-    class Error<T>(data: T? = null, message: String?): State<T>(data = data)
+    class Error<T>(data: T? = null, message: String = "error", status: Int) :
+        State<T>(data = data, message = message, status = status)
 
-    class Interrupted<T>(data: T? = null): State<T>(data = data)
+    class Interrupted<T>(data: T? = null, message: String = "interrupted", status: Int) :
+        State<T>(data = data, message = message, status = status)
 
-    class Processing<T>(data: T? = null): State<T>(data = data)
+    class Processing<T>(data: T? = null, message: String = "processing", status: Int) :
+        State<T>(data = data, message = message, status = status)
 
 
-    class Expired<T>(data: T? = null, message: String?): State<T>(data = data)
+    class Expired<T>(data: T? = null, message: String = "expired", status: Int) :
+        State<T>(data = data, status = status, message = message)
 
 }
---
+++ src/main/kotlin/ru/tanec/cookhelper/core/constants/status/userStatus.kt
@@ -0,0 +1,14 @@
+package ru.tanec.cookhelper.core.constants.status
+
+const val SUCCESS = 100
+const val EXCEPTION = 199
+
+const val USER_NOT_FOUND = 101
+const val WRONG_CREDENTAILS = 102
+const val NOT_VERIFIED = 103
+const val PARAMETER_MISSED = 104
+const val USER_DELETED = 105
+const val PERMISSION_DENIED = 106
+const val NICKNAME_REJECTED = 107
+const val EMAIL_REJECTED = 108
+const val PASSWORD_REJECTED = 109
