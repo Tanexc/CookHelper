9bbb718165762de8190fec188188bb29987c0e1c
Arthur231456 <artur.231456@gmail.com>
2022-10-20 17:33:27 +0300

    Nickname availability routes fix

--- src/main/kotlin/ru/tanec/cookhelper/presentation/features/api/userApi/use_case/NicknameAvailabilityUserUseCase.kt
+++ src/main/kotlin/ru/tanec/cookhelper/presentation/features/api/userApi/use_case/NicknameAvailabilityUserUseCase.kt
@@ -3,6 +3,7 @@ package ru.tanec.cookhelper.presentation.features.api.userApi.use_case
 import io.ktor.http.*
 import ru.tanec.cookhelper.core.constants.REQUIRED
 import ru.tanec.cookhelper.core.constants.status.EMAIL_REJECTED
+import ru.tanec.cookhelper.core.constants.status.NICKNAME_REJECTED
 import ru.tanec.cookhelper.core.constants.status.PARAMETER_MISSED
 import ru.tanec.cookhelper.core.constants.status.SUCCESS
 import ru.tanec.cookhelper.enterprise.model.response.ApiResponse
@@ -19,6 +20,6 @@ object NicknameAvailabilityUserUseCase {
             message = REQUIRED("nickname")
         )
         return if (repository.nicknameAccessibility(nickname)) ApiResponse(status = SUCCESS, data = true, message = "success")
-        else ApiResponse(status = EMAIL_REJECTED, data = false, message = "rejected")
+        else ApiResponse(status = NICKNAME_REJECTED, data = false, message = "rejected")
     }
 }
