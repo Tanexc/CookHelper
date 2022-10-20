e6144cdff5e318a9dbcf6a6b6729295752547fca
Arthur231456 <artur.231456@gmail.com>
2022-10-20 13:32:49 +0300

    application config modules

--- src/main/kotlin/ru/tanec/cookhelper/presentation/app/Application.kt
+++ src/main/kotlin/ru/tanec/cookhelper/presentation/app/Application.kt
@@ -1,6 +1,20 @@
 package ru.tanec.cookhelper.presentation.app
 
+import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import ru.tanec.cookhelper.core.plugins.*
+import ru.tanec.cookhelper.database.factory.DatabaseFactory
+import ru.tanec.cookhelper.presentation.routing.apiRoutes
 
-fun main(args: Array<String>): Unit = EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        DatabaseFactory.init()
+        configureAdministration()
+        configureSockets()
+        configureSerialization()
+        configureRouting()
+        configureDI()
+        apiRoutes()
+    }.start(wait = true)
+}
 
