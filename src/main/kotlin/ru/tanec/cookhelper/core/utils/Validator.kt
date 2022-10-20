e5e919161c2af250810dbc2f14462d923a27b335
Arthur231456 <artur.231456@gmail.com>
2022-10-20 17:48:23 +0300

    Validators fixes

--- src/main/kotlin/ru/tanec/cookhelper/core/utils/Validator.kt
+++ src/main/kotlin/ru/tanec/cookhelper/core/utils/Validator.kt
@@ -3,21 +3,19 @@ package ru.tanec.cookhelper.core.utils
 import ru.tanec.cookhelper.enterprise.utls.Validator.*
 import ru.tanec.cookhelper.enterprise.utls.Validator
 
-object ValidatorImpl: Validator {
+object ValidatorImpl : Validator {
     override fun isValidPassword(password: String): Validity {
-        return if (password.length < 8) {
-            Validity.Invalid("password must be 8 characters long")
-        } else {
-            Validity.Valid()
+        return when (password.length < 8) {
+            true -> Validity.Invalid("password must be 8 characters long")
+            else -> Validity.Valid()
         }
 
     }
 
     override fun isValidLogin(login: String): Validity {
-        return if (!(login.length in 5..15)) {
-            Validity.Invalid("length of login must be in the range from 5 to 15 symbols")
-        } else {
-            Validity.Valid()
+        return when (login.length !in 1..30) {
+            true -> Validity.Invalid("length of login must be in the range from 1 to 30  symbols")
+            else -> Validity.Valid()
         }
     }
 }
