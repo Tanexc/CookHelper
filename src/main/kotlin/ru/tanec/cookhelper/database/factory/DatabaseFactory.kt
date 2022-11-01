f04866c1e7fa116c748ba173c34c786cd24c2ea7
Arthur231456 <artur.231456@gmail.com>
2022-11-02 00:37:04 +0500

    get db static path

--- src/main/kotlin/ru/tanec/cookhelper/database/factory/DatabaseFactory.kt
+++ src/main/kotlin/ru/tanec/cookhelper/database/factory/DatabaseFactory.kt
@@ -10,7 +10,7 @@ import ru.tanec.cookhelper.database.model.*
 object DatabaseFactory {
     fun init() {
         val driverClassName = "org.sqlite.JDBC"
-        val jdbcURL = "jdbc:sqlite:file:./build/db.db"
+        val jdbcURL = "jdbc:sqlite:file:./data/feed/db.db"
         val database = Database.connect(jdbcURL, driverClassName)
         transaction(database) {
             SchemaUtils.create(Users)
