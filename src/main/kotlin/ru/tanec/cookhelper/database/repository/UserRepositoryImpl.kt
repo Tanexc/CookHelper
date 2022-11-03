395566ea74d79dfdc1325ab25ae4e7b4a34fed08
Arthur231456 <artur.231456@gmail.com>
2022-11-03 17:54:14 +0500

    debug

--- src/main/kotlin/ru/tanec/cookhelper/database/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/ru/tanec/cookhelper/database/repository/UserRepositoryImpl.kt
@@ -216,7 +216,7 @@ class UserRepositoryImpl(
     }
 
     override suspend fun getByToken(token: String): Flow<State<User?>> = flow {
-        val user = dao.getByToken(token)?.privateInfo()
+        val user = dao.getByToken(token)
         println("user: $user")
         if (user != null) emit(State.Success(user))
         else emit(State.Error(status=USER_NOT_FOUND))
